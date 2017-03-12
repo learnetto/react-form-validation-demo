e0cad7230421ad0021754c9183bbcb25ec1439f1
Hrishi Mittal <hrishimittal@gmail.com>
2017-03-12 08:08:55 +0000

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
   <App />,
