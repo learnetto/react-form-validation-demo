e8b857b959d8a64bdeabd34e5d76f8450fc5e6f8
Hrishi Mittal <hrishimittal@gmail.com>
2017-03-12 17:31:35 +0000

    Add validation

--- src/Form.js
+++ src/Form.js
@@ -1,32 +1,81 @@
 import React, { Component } from 'react';
+import { FormErrors } from './FormErrors';
 import './Form.css';
 
 class Form extends Component {
-	render () {
-		return (
-			<form className="demoForm">
-				<h2>Sign up</h2>
-				<div className="panel panel-default">
-				  <div className="form-status panel-body">
-				    Form status:
-				  </div>
-				</div>
-			  <div className="form-group">
-			    <label htmlFor="email">Email address</label>
-			    <input type="email" required className="form-control" id="email" placeholder="Email" />
-			  </div>
-			  <div className="form-group">
-			    <label htmlFor="password">Password</label>
-			    <input type="password" className="form-control" id="password" placeholder="Password" />
-			  </div>
-			  <div className="form-group">
-			    <label htmlFor="passwordConfirmation">Confirm Password</label>
-			    <input type="password" className="form-control" id="passwordConfirmation" placeholder="Password" />
-			  </div>
-			  <button type="submit" className="btn btn-primary">Sign up</button>
-			</form>
-		)
-	}
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      formErrors: {email: '', password: ''},
+      formValid: false
+    }
+  }
+
+  handleUserInput (e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value}, () => { this.validateField(name, value) });
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+
+    switch(fieldName) {
+      case 'email':
+        if(!value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+          fieldValidationErrors.email = ' is invalid';
+        } else {
+          fieldValidationErrors.email = '';
+        }
+        break;
+      case 'password':
+        if(value.length < 6) {
+          fieldValidationErrors.password = ' is too short';
+        } else {
+          fieldValidationErrors.password = '';
+        }
+        break;
+      default:
+        break;
+    }
+    this.setState({formErrors: fieldValidationErrors,
+                    formValid: fieldValidationErrors.email.length === 0 &&
+                                fieldValidationErrors.password.length === 0
+                  })
+  }
+
+  errorClass(error) {
+    return(error.length === 0 ? '' : 'has-error');
+  }
+
+  render () {
+    return (
+      <form className="demoForm">
+        <h2>Sign up</h2>
+        <div className="panel panel-default">
+          <FormErrors formErrors={this.state.formErrors} />
+        </div>
+        <div className={`form-group ${this.errorClass(this.state.formErrors.email)}`}>
+          <label htmlFor="email">Email address</label>
+          <input type="email" required className="form-control" name="email"
+            ref={(input) => { this.email = input; }}
+            placeholder="Email"
+            value={this.state.email}
+            onChange={(event) => this.handleUserInput(event)}  />
+        </div>
+        <div className={`form-group ${this.errorClass(this.state.formErrors.password)}`}>
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={(event) => this.handleUserInput(event)}  />
+        </div>
+        <button type="submit" className="btn btn-primary" disabled={!this.state.formValid}>Sign up</button>
+      </form>
+    )
+  }
 }
 
 export default Form;
---
+++ src/FormErrors.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const FormErrors = ({formErrors}) =>
+  <div className='formErrors'>
+    {Object.keys(formErrors).map((fieldName, i) => {
+      if(formErrors[fieldName].length > 0){
+        return (
+          <p key={i}>{fieldName} {formErrors[fieldName]}</p>
+        )        
+      } else {
+        return '';
+      }
+    })}
+  </div>
